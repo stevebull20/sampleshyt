384580343afec7a8db95184271b9cd84725c20e1
stevebull20 <sbull426@gmail.com>
2023-08-20 16:29:48 +0900

    added routes, link some styling

--- src/App.js
+++ src/App.js
@@ -1,38 +1,16 @@
-import Directory from "./directory/directory.component";
-
-const categories = [
-  {
-    id: 1,
-    title: "Hats",
-    imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-  },
-  {
-    id: 2,
-    title: "Jackets",
-    imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-  },
-  {
-    id: 3,
-    title: "Sneakers",
-    imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-  },
-  {
-    id: 4,
-    title: "Womens",
-    imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-  },
-  {
-    id: 5,
-    title: "Mens",
-    imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-  },
-];
+import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>
-      <Directory categories={categories} />
-    </div>
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route />
+        <Route />
+      </Route>
+    </Routes>
   );
 };
 
